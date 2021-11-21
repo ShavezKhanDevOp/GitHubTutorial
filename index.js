5f0caa1b345f01ceea42afc17f528d5ae35c12dc
ShavezKhanDevOp <ShavezKhan.devOp@gmail.com>
2021-11-21 15:05:26 +0530

    small fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile('/index.html'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
